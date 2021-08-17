317cf8a48a717b86fb402a2f79d63aed8868baa6
panda-sas <panda.sas20@gmail.com>
2021-08-17 15:15:15 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.72,
+      date: new Date(2021, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 694.12,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "New Desk(wooden)",
+      amount: 750,
+      date: new Date(2021, 5, 12),
+    },
+    { id: "e4", title: "Wall Colour", amount: 45, date: new Date(2021, 12, 4) },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, {expenses: expenses})
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense />
+      <Expenses expenses= {expenses} />
+
     </div>
   );
 }
